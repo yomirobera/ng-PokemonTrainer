d2b1e31f53adf12067846fc0eadde150c3df23b6
“William <vilhelmsennn@gmail.com>
2023-02-28 13:46:56 +0100

    Added pokemon pictures

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -3,3 +3,10 @@ export interface Pokemon {
     name: string;
     url: string;  
 }
+
+export interface PokeResults {
+    count: number;
+    next: string;
+    previous: string;
+    results: Pokemon[];
+}
